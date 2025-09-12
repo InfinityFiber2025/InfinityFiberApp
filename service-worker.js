77fe6e8c3813f932e95e15467742498d076c59e6
InfinityFiber2025 <rogerioramosgear@gmail.com>
2025-09-12 20:25:10 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,5 @@
-const CACHE_NAME = "infinity-cache-v1";
-const urlsToCache = ["/", "/index.html", "/manifest.json"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(cacheNames.map((cache) => {
-        if (cache !== CACHE_NAME) return caches.delete(cache);
-      }))
-    )
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
-});
+const CACHE='ifiber-v40-cache-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
