dc84ca9beb3c356e0330e5185604f5e46963b015
InfinityFiber2025 <rogerioramosgear@gmail.com>
2025-09-16 15:23:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 function login(){
   let user = document.getElementById('usuario').value;
   let pass = document.getElementById('senha').value;
-  if(user=="" || pass==""){
-    alert("Preencha usuário e senha para continuar.");
+  if(user!=="DanielKascher" || pass!=="K@scher123"){
+    alert("Usuário ou senha incorretos.");
     return;
   }
   sessionStorage.setItem('banco','Banco 01');
   sessionStorage.setItem('agencia','0001');
   sessionStorage.setItem('saldo',50000000);
-  window.location='dashboard.html';
+  sessionStorage.setItem('usuario', user);
+  window.location='welcome.html';
 }
 function toggleSenha(){
   let f = document.getElementById('senha');
