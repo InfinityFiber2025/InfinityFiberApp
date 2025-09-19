3da7651a4d85aace955d5b41c24dd6dcf40cd7d1
InfinityFiber2025 <rogerioramosgear@gmail.com>
2025-09-18 23:27:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
 self.addEventListener('install', e=>{
   e.waitUntil(
-    caches.open('infinity-cache-v50').then(cache=>{
-      return cache.addAll(['index.html','style.css','app.js','manifest.json']);
-    })
+    caches.open('infinity-cache-v52').then(cache=> cache.addAll(['index.html','style.css','app.js','manifest.json']))
   );
 });
 self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(resp=>resp || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
 });
