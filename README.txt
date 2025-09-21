8551adb1af637895d04e6e284d47c20c80242e7e
InfinityFiber2025 <rogerioramosgear@gmail.com>
2025-09-21 11:04:30 -0300

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,5 +1,1 @@
-InfinityFiber v10.3 - Admin + Cliente + Biometria
-Instruções:
-1. npm install
-2. npm start
-3. npm run export:web para gerar web-build.
+Versão final do protótipo Infinity Bank.
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('infinity-bank-cache').then(cache => {
-      return cache.addAll([ '/', '/index.html', '/manifest.json' ]);
-    })
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
-});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request))});
