9b9a56fd357a3b6a6e36a475fc06420168c8f1ef
InfinityFiber2025 <rogerioramosgear@gmail.com>
2025-09-22 19:31:34 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/InfinityFiberApp/',   // 👈 obrigatório para GitHub Pages
+  base: '/InfinityFiberApp/', // obrigatório para GitHub Pages
 })
